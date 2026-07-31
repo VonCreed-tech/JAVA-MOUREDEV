ffb7cda2ba60742dc9b3101cf3b6202f52ce76b3
Ignacio Madriaga <ing.ignaciomadriaga@gmail.com>
2026-07-30 23:50:20 -0400

    wip: exercising with moidifers of access

---
+++ AccesModificators/classes.java
@@ -0,0 +1,5 @@
+package AccesModificators;
+
+public class classes {
+    
+}
--- OOP/Person.java
+++ OOP/Person.java
@@ -1,8 +1,8 @@
 package OOP;
 
 public class Person {
-    String name;
-    int age;
+    private String name;
+    private int age;
 
     public Person(String name, int age){
         this.name = name;
